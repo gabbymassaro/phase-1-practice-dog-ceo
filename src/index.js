c4e079d49af2e57a090c12b2850a9317acd5158e
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-21 09:06:51 -0400

    Complete Challenge 2
    On page load, fetches all the dog breeds and appends them to the page in the <ul>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", (e) => {
 e.preventDefault()
 const dogPark = document.getElementById("dog-image-container")
-console.log(dogPark)
+const dogBreeds = document.getElementById("dog-breeds")
+console.log(dogBreeds)
 
 fetch("https://dog.ceo/api/breeds/image/random/4")
   .then(function(response) {
@@ -10,10 +11,22 @@ fetch("https://dog.ceo/api/breeds/image/random/4")
   .then(function (data) {
     let dogArray = data.message
     dogArray.forEach(function (dogs) {
-      console.log(dogs)
       const image = document.createElement('img')
       image.src = dogs
       dogPark.appendChild(image)
+    })
+  })
+
+fetch("https://dog.ceo/api/breeds/list/all")
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function (data) {
+    dogBreedArray = Object.keys(data.message)
+    dogBreedArray.forEach(function (breed){ 
+      const li = document.createElement('li')
+      li.textContent = `${breed}`
+      dogBreeds.appendChild(li)
     })
   })
 })
