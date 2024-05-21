d73adcc0333026008bb53d01379e41ce3be51d75
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-21 08:44:19 -0400

    Complete Challenge 1
    load images on page load, parse the response as JSON, add image elements to the DOM for each image in the array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", (e) => {
+e.preventDefault()
+const dogPark = document.getElementById("dog-image-container")
+console.log(dogPark)
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function (data) {
+    let dogArray = data.message
+    dogArray.forEach(function (dogs) {
+      console.log(dogs)
+      const image = document.createElement('img')
+      image.src = dogs
+      dogPark.appendChild(image)
+    })
+  })
+})
